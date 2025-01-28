1433cd92233bfff804412488a13569f3c8452f0c
muhammed shafi ek <muhammadshafimhdshafi@gmail.com>
2025-01-28 15:29:16 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-#Title of the web app
-st.title("my first stramlit App")
-# Text input for the user
-name=st.text_input("enter you namee:")
-# Butten no triger an action
-if st.button("say hello"):
-    st.write(f"hello,{name}!")
